26d850a6181e32cbb9fede814c3c15831ad6d396
Eric Kalkanger <eanger@gmail.com>
2022-01-21 16:55:39 -0600

    2021 day 22: p2 i think working

--- 2021/day22.py
+++ 2021/day22.py
@@ -15,22 +15,24 @@ for line in lines:
                       int(m[4]), int(m[5]),
                       int(m[6]), int(m[7])))
 
-world = set()
-for (direction, xmin, xmax, ymin, ymax, zmin, zmax) in steps[:20]:
-    for x in range(xmin, xmax + 1):
-        for y in range(ymin, ymax + 1):
-            for z in range(zmin, zmax + 1):
-                if -50 <= x <= 50 and \
-                    -50 <= y <= 50 and \
-                    -50 <= z <= 50:
-                    if direction == 'on':
-                        # add elements to world
-                        world.add((x, y, z))
-                    else:
-                        # remove elements from world
-                        world.discard((x, y, z))
+do_part_1 = False
+if do_part_1:
+    world = set()
+    for (direction, xmin, xmax, ymin, ymax, zmin, zmax) in steps[:20]:
+        for x in range(xmin, xmax + 1):
+            for y in range(ymin, ymax + 1):
+                for z in range(zmin, zmax + 1):
+                    if -50 <= x <= 50 and \
+                        -50 <= y <= 50 and \
+                        -50 <= z <= 50:
+                        if direction == 'on':
+                            # add elements to world
+                            world.add((x, y, z))
+                        else:
+                            # remove elements from world
+                            world.discard((x, y, z))
 
-print(len(world))
+    print(len(world))
 
 # part 2
 _ = '''
@@ -41,19 +43,63 @@ when inserting cuboid, just add to collection
 
 when removing, have to do a collision check with each cuboid
 collision:
-true for each dimension: 
+true for each dimension:
 self = b, other = a
 return (a.minX <= b.maxX && a.maxX >= b.minX) &&
        (a.minY <= b.maxY && a.maxY >= b.minY) &&
        (a.minZ <= b.maxZ && a.maxZ >= b.minZ)
 if they collide, need to remove colliding cuboid and recreate cuboids in void
-(a.minx, b.minx), (a.miny, b.miny)
-(b.minx, a.maxx), (a.miny, b.miny)
-(a.minx, b.minx), (b.miny, a.maxy)
-if left>=right for any dimension above, that cuboid is contained
 
+in three dimensions, there are up to 27 subdivisions of a cuboid on intersection
+one of those will by necessity be the intersected cuboid (self)
+so up to 26 new cuboids will be made
+
+for 1 dimension, assuming collision:
+1 amin bmin: amin <= bmin
+2 max(amin, bmin) min(amax, bmax)
+3 bmax amax: bmax <= amax
+
+for 2 dimensions, if we assume collision(could be reduced?):
+collision:
+if axmin <= bxmin:
+1 (axmin, aymin) (bxmin, bymin): aymin <= bymin
+2 (axmin, max(bymin, aymin)) (bxmin, min(bymax, aymax))
+3 (axmin, bymax) (bxmin, aymax): aymax >= bymax
+
+4 (bxmin, aymin) (bxmax, bymin): bymin >= aymin
+5 (max(axmin, bxmin), max(aymin, bymin)) (min(axmax, bxmax), min(aymax, bymax))
+6 (bxmin, bymax) (bxmax, aymax): aymax >= bymax
+
+if bxmax <= axmax:
+7 (bxmax, aymin) (axmax, bymin): aymin <= bymin
+8 (bxmax, max(bymin, aymin)) (axmax, min(bymax, aymax))
+9 (bxmax, bymax) (axmax, aymax): aymax >= bymax
+
+
+for dim in [x, y, z]:
+rules ^ (num dimension)
+rules = [lambda a, b: (a.min, b.min) if a.min <= b.min else False,
+         lambda _, _: (max(a.min, b.min), min(a.max, b.max)),
+         lambda a, b: (b.max, a.max) if a.max <= b.max else False]
+for xrule in rules:
+  for yrule in rules:
+    for zrule in rules:
+      if vals := all(xrule(a.x, b.x), yrule(a.y, b.y), zrule(a.z, b.z)):
+        new_cuboid(vals[0], vals[1], vals[2]) vals[i] is pair of min, max for that dim
+
+if a[dim].min <= b[dim].min: (a[dim].min, b[dim].min)
+(max(a[dim].min, b[dim].min), min(a[dim].max, b[dim].max))
+if b[dim].max <= a[dim].max: (b[dim].max, a[dim].max)
+
+ALL DIMENSIONS ARE INCLUSIVE
 '''
 from dataclasses import dataclass
+
+
+rules = [lambda amin, amax, bmin, bmax: (amin, bmin) if amin <= bmin else False,
+         lambda amin, amax, bmin, bmax: (max(amin, bmin), min(amax, bmax)),
+         lambda amin, amax, bmin, bmax: (bmax, amax) if bmax <= amax else False]
+
 
 @dataclass(frozen=True)
 class Cuboid:
@@ -65,15 +111,29 @@ class Cuboid:
     zmax: int
 
     def num_on(self):
-        return (self.xmax - self.xmin) * \
-            (self.ymax - self.ymin) * \
-            (self.zmax - self.zmin)
+        return ((self.xmax - self.xmin) *
+                (self.ymax - self.ymin) *
+                (self.zmax - self.zmin))
 
-    def intersections(self, other):
+    def differences(self, other):
+        '''
+        Returns a list of all cuboids contained by other that aren't
+        part of self.
+        '''
+        res = []
         if (other.xmin <= self.xmax and other.xmax >= self.xmin and
             other.ymin <= self.ymax and other.ymax >= self.ymin and
             other.zmin <= self.zmax and other.zmax >= self.zmin):
-            pass
+            for xidx, xrule in enumerate(rules):
+                if xvals := xrule(self.xmin, self.xmax, other.xmin, other.xmax):
+                    for yidx, yrule in enumerate(rules):
+                        if yvals := yrule(self.ymin, self.ymax, other.ymin, other.ymax):
+                            for zidx, zrule in enumerate(rules):
+                                if xidx == yidx == zidx == 1:
+                                    continue  # we really don't want the middle
+                                if zvals := zrule(self.zmin, self.zmax, other.zmin, other.zmax):
+                                    res.append(Cuboid(*xvals, *yvals, *zvals))
+        return res
 
 
 cuboids: list[Cuboid] = []
@@ -81,7 +141,7 @@ for (direction, xmin, xmax, ymin, ymax, zmin, zmax) in steps:
     step_cuboid = Cuboid(xmin, xmax, ymin, ymax, zmin, zmax)
     new_cuboids = []
     for cuboid in cuboids:
-        if to_add := step_cuboid.intersections(cuboid):
+        if to_add := step_cuboid.differences(cuboid):
             new_cuboids.extend(to_add)
         else:
             new_cuboids.append(cuboid)
