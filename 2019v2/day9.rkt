690e327ed4d2cc8575dcb01a24bdd64c8e3f31c6
ekalkanger <ekalkanger@pinterest.com>
2022-12-05 10:56:50 -0600

    2019v2 day 9 p1 rel base addressing

--- 2019v2/day9.rkt
+++ 2019v2/day9.rkt
@@ -3,72 +3,60 @@
 
 (module+ test
   (require rackunit)
+  (check-equal? (intcode-ref-mode (make-intcode #(203 0)) 0) 203)
+  (check-equal? (intcode-ref-mode (make-intcode #(203 1)) 0) 1)
+  (check-equal? (intcode-ref-mode (intcode #(203 0 0 0 123) 0 #f #f 4) 0) 123)
   )
 
-(struct intcode (program ip input output) #:transparent)
+(struct intcode (program ip input output base) #:transparent)
+
+(define (make-intcode prog)
+  (intcode prog 0 #f #f 0))
 
 (define (intcode-ref vm [offset 0])
   (vector-ref (intcode-program vm) (+ (intcode-ip vm) offset)))
+
 (define (intcode-ref-absolute vm absolute)
   (vector-ref (intcode-program vm) absolute))
+
+(define (intcode-ref-mode vm param-num)
+  (let* ([val (intcode-ref vm (add1 param-num))]
+         [instr (intcode-ref vm)]
+         [all-modes (quotient instr 100)]
+         [mode (remainder (quotient all-modes (expt 10 param-num)) 10)])
+    (match mode
+      [0 (intcode-ref-absolute vm val)]
+      [1 val]
+      [2 (intcode-ref-absolute vm (+ val (intcode-base vm)))]
+      [_ (println "BAD MODE") #f])))
 
 (define (intcode-halted? vm)
   (= 99 (intcode-ref vm)))
 
-(define (binary-op op vm modes)
-    (let* ([left-idx (intcode-ref vm 1)]
-           [right-idx (intcode-ref vm 2)]
-           [left (value-by-mode vm left-idx modes 0)]
-           [right (value-by-mode vm right-idx modes 1)]
-           [dest (intcode-ref vm 3)]
-           [next-ip (+ (intcode-ip vm) 4)])
-      (vector-set! (intcode-program vm) dest (op left right))
+(define (binary-op op left right dest vm)
+    (let ([res (op left right)]
+          [next-ip (+ (intcode-ip vm) 4)])
+      (vector-set! (intcode-program vm) dest res)
       (process (struct-copy intcode vm [ip next-ip]))))
 
-(define (read-input vm)
+(define (read-input addr vm)
   (if (intcode-input vm)
-      (let ([addr (intcode-ref vm 1)])
+      (begin
         (vector-set! (intcode-program vm) addr (intcode-input vm))
         (process (struct-copy intcode vm
                               [ip (+ (intcode-ip vm) 2)]
                               [input #f])))
       vm))
 
-(define (write-output vm modes)
-  (let* ([addr (intcode-ref vm 1)]
-         [val (value-by-mode vm addr modes 0)])
-    (process (struct-copy intcode vm
-                          [ip (+ (intcode-ip vm) 2)]
-                          [output val]))))
+(define (write-output val vm)
+  (process (struct-copy intcode vm
+                        [ip (+ (intcode-ip vm) 2)]
+                        [output val])))
 
-(define (jump-if pred vm modes)
-  (let* ([cond-idx (intcode-ref vm 1)]
-         [dest-idx (intcode-ref vm 2)]
-         [cond-val (value-by-mode vm cond-idx modes 0)]
-         [dest-val (value-by-mode vm dest-idx modes 1)])
-    (process (struct-copy intcode vm [ip (if (pred cond-val)
-                                             dest-val
-                                             (+ (intcode-ip vm) 3))]))))
-
-(define (less-than vm modes)
-  (let* ([left-idx (intcode-ref vm 1)]
-         [right-idx (intcode-ref vm 2)]
-         [left (value-by-mode vm left-idx modes 0)]
-         [right (value-by-mode vm right-idx modes 1)]
-         [dest (intcode-ref vm 3)]
-         [next-ip (+ (intcode-ip vm) 4)])
-    (vector-set! (intcode-program vm) dest (if (< left right) 1 0))
-    (process (struct-copy intcode vm [ip next-ip]))))
-
-(define (is-equal vm modes)
-  (let* ([left-idx (intcode-ref vm 1)]
-         [right-idx (intcode-ref vm 2)]
-         [left (value-by-mode vm left-idx modes 0)]
-         [right (value-by-mode vm right-idx modes 1)]
-         [dest (intcode-ref vm 3)]
-         [next-ip (+ (intcode-ip vm) 4)])
-    (vector-set! (intcode-program vm) dest (if (= left right) 1 0))
-    (process (struct-copy intcode vm [ip next-ip]))))
+(define (jump-if predicate cond-val dest-val vm)
+  (process (struct-copy intcode vm [ip (if (predicate cond-val)
+                                           dest-val
+                                           (+ (intcode-ip vm) 3))])))
 
 (define (halt vm)
   (unless (intcode-halted? vm)
@@ -76,26 +64,25 @@
   ;; (println vm)
   vm)
 
-(define (value-by-mode vm val modes param-idx)
-  (let-values ([(mode-1 mode-0) (quotient/remainder modes 10)])
-    (match (if (equal? param-idx 0) mode-0 mode-1)
-      [0 (intcode-ref-absolute vm val)]
-      [1 val]
-      [_ (println "BAD MODE") #f])))
+(define (eq0 x) (= x 0))
+(define (neq0 x) (not (eq0 x)))
+(define (less-than left right) (if (< left right) 1 0))
+(define (is-equal left right) (if (= left right) 1 0))
 
-; input (input output) ... terminate
-; return an optional output on input.
 (define (process vm)
-  (let-values ([(modes opcode) (quotient/remainder (intcode-ref vm) 100)])
+  (let ([opcode (remainder (intcode-ref vm) 100)]
+        [p1 (intcode-ref-mode vm 0)]
+        [p2 (intcode-ref-mode vm 1)]
+        [p3 (intcode-ref-mode vm 2)])
     (match opcode
-      [1 (binary-op + vm modes)]
-      [2 (binary-op * vm modes)]
-      [3 (read-input vm)]
-      [4 (write-output vm modes)]
-      [5 (jump-if (lambda (x) (not (= x 0))) vm modes)]
-      [6 (jump-if (lambda (x) (= x 0)) vm modes)]
-      [7 (less-than vm modes)]
-      [8 (is-equal vm modes)]
+      [1 (binary-op + p1 p2 p3 vm)]
+      [2 (binary-op * p1 p2 p3 vm)]
+      [3 (read-input p1 vm)]
+      [4 (write-output p1 vm)]
+      [5 (jump-if eq0 p1 p2 vm)]
+      [6 (jump-if neq0 p1 p2 vm)]
+      [7 (binary-op less-than p1 p2 p3 vm)]
+      [8 (binary-op is-equal p1 p2 p3 vm)]
       [99 (halt vm)]
       [other (printf "UNIMPLEMENTED OPCODE: ~a\n" other) vm])))
 
