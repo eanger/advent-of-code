d57a36aaa5fb9720ce3b7c7b623321d38b6d1a5c
ekalkanger <ekalkanger@pinterest.com>
2022-12-06 12:15:45 -0600

    2019v2 day 9 p1

--- 2019v2/day9.rkt
+++ 2019v2/day9.rkt
@@ -1,6 +1,9 @@
 #lang racket
 (require "utils.rkt")
 
+(define debug #f)
+(define printf (if debug printf void))
+
 (module+ test
   (require rackunit)
   (check-equal? (intcode-ref-mode (make-intcode #(203 0)) 0 #t) 203)
@@ -34,12 +37,13 @@
          [instr (intcode-ref vm)]
          [all-modes (quotient instr 100)]
          [mode (remainder (quotient all-modes (expt 10 param-num)) 10)])
-    ;; (printf "~a ~a ~a ~a\n" val instr all-modes mode)
+    (printf "~a ~a ~a ~a ~a\n" val instr all-modes mode (intcode-base vm))
     (match mode
       [0 (if deref? (intcode-ref-absolute vm val) val)]
       [1 val]
-      [2 (if deref? (intcode-ref-absolute vm (+ val (intcode-base vm))) val)]
-      [_ (println "BAD MODE") #f])))
+      [2 (let ([rel-val (+ val (intcode-base vm))])
+           (if deref? (intcode-ref-absolute vm rel-val) rel-val))]
+      [_ (eprintf "BAD MODE\n") #f])))
 
 (define (intcode-halted? vm)
   (= 99 (intcode-ref vm)))
@@ -55,8 +59,10 @@
     (process (struct-copy intcode vm [ip next-ip]))))
 
 (define (read-input vm)
+  (printf "BEFORE input: ~a base: ~a\n" (intcode-input vm) (intcode-base vm))
   (if (intcode-input vm)
       (let ([addr (intcode-ref-mode vm 0 #f)])
+        (printf "input: ~a addr: ~a base: ~a\n" (intcode-input vm) addr (intcode-base vm))
         (intcode-program-write vm addr (intcode-input vm))
         (process (struct-copy intcode vm
                               [ip (+ (intcode-ip vm) 2)]
@@ -72,19 +78,22 @@
 (define (jump-if predicate vm)
   (let ([cond-val (intcode-ref-mode vm 0 #t)]
         [dest-val (intcode-ref-mode vm 1 #t)])
+    (printf "cond: ~a dest: ~a\n" cond-val dest-val)
     (process (struct-copy intcode vm [ip (if (predicate cond-val)
                                              dest-val
                                              (+ (intcode-ip vm) 3))]))))
 
 (define (rel-base vm)
-  (let ([increment (intcode-ref-mode vm 0 #t)])
+  (let* ([increment (intcode-ref-mode vm 0 #t)]
+         [newbase (+ (intcode-base vm) increment)])
+    (printf "base: ~a inc: ~a newbase: ~a\n" (intcode-base vm) increment newbase)
     (process (struct-copy intcode vm
                           [ip (+ (intcode-ip vm) 2)]
-                          [base (+ (intcode-base vm) increment)]))))
+                          [base newbase]))))
 
 (define (halt vm)
   (unless (intcode-halted? vm)
-    (println "ERROR: VM not halted correctly"))
+    (eprintf "ERROR: VM not halted correctly\n"))
   vm)
 
 (define (eq0 x) (= x 0))
@@ -92,8 +101,21 @@
 (define (less-than left right) (if (< left right) 1 0))
 (define (is-equal left right) (if (= left right) 1 0))
 
+(define decoder #hash((1 . "add")
+                      (2 . "mul")
+                      (3 . "read")
+                      (4 . "write")
+                      (5 . "jump-neq0")
+                      (6 . "jump-eq0")
+                      (7 . "less-than?")
+                      (8 . "is-equal?")
+                      (9 . "rel-base")
+                      (99 . "halt")))
+
 (define (process vm)
-  (let ([opcode (remainder (intcode-ref vm) 100)])
+  (let* ([opcode (remainder (intcode-ref vm) 100)]
+         [op (hash-ref decoder opcode)])
+    (printf "ip: ~a op: ~a\n" (intcode-ip vm) op)
     (match opcode
       [1 (binary-op + vm)]
       [2 (binary-op * vm)]
