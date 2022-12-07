8eef053302d3ec27555b9661bb0ad46fb047a693
ekalkanger <ekalkanger@pinterest.com>
2022-12-07 10:08:06 -0600

    2019v2 day10 redo with polar coords

--- 2019v2/day10.rkt
+++ 2019v2/day10.rkt
@@ -77,11 +77,23 @@
 ...##")
 
 (struct point (x y) #:transparent)
-(define (point-normalize p origin)
-  (let* ([diff-x (- (point-x p) (point-x origin))]
-         [diff-y (- (point-y p) (point-y origin))]
-         [divisor (gcd diff-x diff-y)])
-    (cons divisor (point (/ diff-x divisor) (/ diff-y divisor)))))
+
+(struct polar (r theta) #:transparent)
+
+(define (to-polar p)
+  (let ([r (sqrt (+ (sqr (point-x p))
+                    (sqr (point-y p))))]
+        [theta (if (= (point-x p) 0)
+                   (/ pi 2)
+                   (atan (/ (point-y p) (point-x p))))])
+    (polar r (radians->degrees (cond
+                                 [(< (point-x p) 0) (+ theta pi)]
+                                 [(< (point-y p) 0) (+ theta (* 2 pi))]
+                                 [else theta])))))
+
+(define (point-rel p origin)
+  (point (- (point-x p) (point-x origin))
+         (- (point-y p) (point-y origin))))
 
 (define (parse-image img-str)
   (for/list ([line (string-split img-str "\n")]
@@ -92,20 +104,21 @@
              #:when (char=? char #\#))
     (point x y)))
 
+(define (eval-asteroid candidate asteroids)
+ (set-count (list->set
+             (map polar-theta
+                  (map (lambda (x) (to-polar (point-rel x candidate))) asteroids)))))
+
 (define (asteroids-visible img-str)
   (let ([asteroids (parse-image img-str)])
     (for/list ([asteroid (in-list asteroids)])
-      (cons asteroid (eval-asteroid asteroid asteroids)))))
-
-(define (eval-asteroid candidate asteroids)
-  (set-count
-   (for/set ([asteroid (in-list asteroids)]
-              #:unless (equal? asteroid candidate))
-     (cdr
-      (point-normalize asteroid candidate))
-     )))
+      (cons asteroid (eval-asteroid asteroid (remove asteroid asteroids))))))
 
 (define (max-visible img-str)
   (apply max (map cdr (asteroids-visible img-str))))
 
 (max-visible (file->string "input.day10"))
+
+; part 2
+
+; order closest asteroids before removing from set
