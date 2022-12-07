f91d7159c3c8c7ed73bae475cc8cf9c0a655a0fe
ekalkanger <ekalkanger@pinterest.com>
2022-12-07 13:56:28 -0600

    2022 day2 part1

---
+++ 2022/day2.rkt
@@ -0,0 +1,36 @@
+#lang racket
+
+(module+ test
+  (require rackunit)
+  (check-equal? (total-score "A Y
+B X
+C Z") 15))
+
+(define (total-score guide)
+  (let ([games (parse-guide guide)])
+    (apply + (map score-game games))))
+
+(define (parse-guide guide)
+  (map string-split
+       (string-split guide "\n")))
+
+(define (score-game game)
+  (define winner-score #hash((("A" "X") . 3) ; draw 3
+                             (("A" "Y") . 6) ; win 6
+                             (("A" "Z") . 0) ; lose 0
+                             (("B" "X") . 0) ; lose 0
+                             (("B" "Y") . 3) ; draw 3
+                             (("B" "Z") . 6) ; win 6
+                             (("C" "X") . 6) ; win 6
+                             (("C" "Y") . 0) ; lose 0
+                             (("C" "Z") . 3) ; draw 3
+                             ))
+
+  (define shape-score #hash(("X" . 1)
+                            ("Y" . 2)
+                            ("Z" . 3)))
+  (+
+   (hash-ref shape-score (second game))
+   (hash-ref winner-score game)))
+
+(total-score (file->string "input.day2"))
