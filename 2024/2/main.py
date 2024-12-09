9573c9ce31af38a0facd7611f838529c28564f27
ekalkanger <ekalkanger@pinterest.com>
2024-12-09 15:48:21 -0600

    day2 part2

--- 2024/2/main.py
+++ 2024/2/main.py
@@ -2,6 +2,7 @@
 
 import util
 import operator
+import itertools
 
 
 p1_example = """7 6 4 2 1
@@ -30,11 +31,20 @@ def part1(inp):
     return len([y for y in filter(safep, map(lambda x: [y for y in map(int, x.split())], reports))])
 
 
-def part2():
-    pass
+def part2(inp):
+    reports = inp.splitlines()
+    candidates = map(lambda x: [y for y in map(int, x.split())], reports)
+    res = 0
+    for candidate in candidates:
+        combs = [candidate]
+        combs.extend(itertools.combinations(candidate, len(candidate) - 1))
+        if any(filter(safep, combs)):
+            res += 1
+    return res
 
 
 # ------------------------------------------------
 print(part1(p1_example))
 print(part1(util.input_str()))
-print(part2())
+print(part2(p1_example))
+print(part2(util.input_str()))
