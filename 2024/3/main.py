5bb41f54aeaea0c82c0091593df0d28710aed9b1
ekalkanger <ekalkanger@pinterest.com>
2024-12-11 12:38:08 -0600

    day3 p2

--- 2024/3/main.py
+++ 2024/3/main.py
@@ -5,6 +5,7 @@ import itertools
 
 
 part1_example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+part2_example = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
 
 
 def parse_text(inp, text):
@@ -44,14 +45,47 @@ def parse_exp(inp):
     return (int(n1), int(n2)), idx
 
 
-def parse(inp):
+def parse_do(inp):
+    if inp == '':
+        return None, 1
+
+    if parse_text(inp, "do()") == "do()":
+        return True, len("do()")
+
+    return None, 1
+
+
+def parse_dont(inp):
+    if inp == '':
+        return None, 1
+
+    if parse_text(inp, "don't()") == "don't()":
+        return True, len("don't()")
+
+    return None, 1
+
+
+def parse(inp, part2=False):
     idx = 0
     length = len(inp)
     res = []
+    enabled = True
     while idx < length:
         p, n = parse_exp(inp[idx:])
         if p:
-            res.append(p)
+            if enabled:
+                res.append(p)
+            idx += n
+            continue
+        if part2:
+            if enabled:
+                p, n = parse_dont(inp[idx:])
+                if p:
+                    enabled = False
+            else:
+                p, n = parse_do(inp[idx:])
+                if p:
+                    enabled = True
         idx += n
     return res
 
@@ -64,10 +98,12 @@ def part1(inp):
 
 
 def part2(inp):
-    pass
+    res = parse(inp, True)
+    return sum(x * y for (x, y) in res)
 
 
 # ------------------------------------------------
 print(part1(part1_example))
 print(part1(util.input_str()))
-part2(part1_example)
+print(part2(part2_example))
+print(part2(util.input_str()))
