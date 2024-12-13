6caec612d4a859326f475f539e436beb9a6083ea
ekalkanger <ekalkanger@pinterest.com>
2024-12-13 10:38:03 -0600

    day5 p2

--- 2024/5/main.py
+++ 2024/5/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import util
+import itertools
 
 
 part1_example = """47|53
@@ -31,7 +32,7 @@ part1_example = """47|53
 75,97,47,61,53
 61,13,29
 97,13,75,29,47"""
-part2_example = ''
+part2_example = part1_example
 
 
 def parse(inp):
@@ -44,13 +45,19 @@ def parse(inp):
     return rules, updates
 
 
-def update_value(update, rules):
+def check_value(update, rules):
     for i in range(len(update)):
         candidate = update[i]
         left = set(update[:i])
-        if any(x in left for x in rules.get(candidate, set())):
-            return 0
-    return update[len(update) // 2]
+        if any(x in left for x in rules.get(candidate, [])):
+            return False
+    return True
+
+
+def update_value(update, rules):
+    if check_value(update, rules):
+        return update[len(update) // 2]
+    return 0
 
 
 def part1(inp):
@@ -63,11 +70,45 @@ def part1(inp):
     return sum(update_value(x, rules) for x in updates)
 
 
+def fix_update(update, rules):
+    """
+    if something is in left that shouldn't be, remove it and place it just to the right
+
+    75,97,47,61,53
+       ^ should have 75 come after it
+
+    would become
+    97,75,47,61,53
+
+    """
+    i = 0
+    while i < len(update):
+        candidate = update[i]
+        left = update[:i]
+        to_swap = -1
+        for rule in rules.get(candidate, []):
+            try:
+                to_swap = left.index(rule)
+                break
+            except ValueError:
+                pass
+        if to_swap != -1:
+            update = update[:to_swap] + update[to_swap+1:i] + [candidate, update[to_swap]] + update[i+1:]
+            i -= 1
+        else:
+            i += 1
+
+    return update[len(update) // 2]
+
 def part2(inp):
-    pass
+    rules, updates = parse(inp)
+    values = [update_value(x, rules) for x in updates]
+    to_fix = [a for a, b in zip(updates, values) if b == 0]
+    return sum(fix_update(x, rules) for x in to_fix)
 
 
 # ------------------------------------------------
 print(part1(part1_example))
 print(part1(util.input_str()))
 print(part2(part2_example))
+print(part2(util.input_str()))
