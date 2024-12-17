a32db4b766f9f73a66e30872d188d3e5374fac26
ekalkanger <ekalkanger@pinterest.com>
2024-12-17 14:17:09 -0600

    day9 p2

--- 2024/9/main.py
+++ 2024/9/main.py
@@ -5,7 +5,7 @@ import operator
 
 
 part1_example = '2333133121414131402'
-part2_example = ''
+part2_example = part1_example
 
 
 def part1(inp):
@@ -53,10 +53,82 @@ def part1(inp):
 
 
 def part2(inp):
-    pass
+    '''
+    similar walk
+    if vals[idx] >= vals[replace_idx]: add and decrease vals[idx] by vals[replace_idx]
+
+
+2333133121414131402
+
+00...111...2...333.44.5555.6666.777.888899
+0099.111...2...333.44.5555.6666.777.8888..
+0099.1117772...333.44.5555.6666.....8888..
+0099.111777244.333....5555.6666.....8888..
+00992111777.44.333....5555.6666.....8888..
+
+    {2, 0, not moved}
+    {3, -1, not moved}
+    {3, 1, not moved}
+    {3, -1, not moved}...
+
+    {2, 0, not moved}
+    {2, 9, moved}
+    {1, 2, moved}...
+
+    convert one list to another until convergence
+    convergence when, walking right to left, no more changes can be made
+    '''
+    class Block():
+        def __init__(self, length, ID):
+            self.length = length
+            self.ID = ID
+            self.moved = False
+        def __repr__(self):
+            return f'({self.length}, {self.ID}, {self.moved})'
+
+    vals = list(map(int, inp))
+    blocks = []
+    for idx, v in enumerate(vals):
+        this_id = -1
+        if idx % 2 == 0:
+            this_id = idx // 2
+        blocks.append(Block(v, this_id))
+
+    max_replace_idx = len(vals) - 1
+    if len(vals) % 2 == 0:
+        max_replace_idx -= 1
+
+    idx = max_replace_idx
+    while idx != 0:
+        if blocks[idx].ID != -1 and not blocks[idx].moved:
+            # walk from idx backwards to see if it could move anywhere
+            for replace_idx in range(idx):
+                if blocks[replace_idx].ID != -1:
+                    continue
+                if blocks[replace_idx].length >= blocks[idx].length:
+                    # do move
+                    new_b = Block(blocks[idx].length, blocks[idx].ID)
+                    blocks[replace_idx].length -= blocks[idx].length
+                    blocks[idx].ID = -1
+                    blocks = blocks[:replace_idx] + [new_b] + blocks[replace_idx:]
+                    break
+        idx -= 1
+
+    # checksum
+    res = []
+    for b in blocks:
+        if b.ID == -1:
+            res.extend([0] * b.length)
+        else:
+            res.extend([b.ID] * b.length)
+    acc = 0
+    for i, v in enumerate(res):
+        acc += i * v
+    return acc
 
 
 # ------------------------------------------------
 print(part1(part1_example))
 print(part1(util.input_str().strip()))
 print(part2(part2_example))
+print(part2(util.input_str().strip()))
